8369c3bb876b1c1770b1a8d133058c17e28596bc
acevedosharp <acevedosharp@hotmail.com>
2020-11-11 12:07:54 -0500

    Failing ensemble

--- src/main/java/xyz/acevedosharp/EpicEnsembleEvaluator.java
+++ src/main/java/xyz/acevedosharp/EpicEnsembleEvaluator.java
@@ -2,6 +2,9 @@ package xyz.acevedosharp;
 
 import ai.libs.jaicore.components.api.IComponentInstance;
 import ai.libs.jaicore.components.model.ComponentInstance;
+import ai.libs.jaicore.ml.weka.dataset.WekaInstances;
+import org.api4.java.ai.ml.core.dataset.splitter.SplitFailedException;
+import org.api4.java.ai.ml.core.dataset.supervised.ILabeledDataset;
 import org.api4.java.common.attributedobjects.IObjectEvaluator;
 import weka.classifiers.Classifier;
 import ai.libs.jaicore.ml.weka.WekaUtil;
@@ -9,72 +12,61 @@ import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.UnsupportedAttributeTypeException;
 
-import java.io.File;
-import java.io.FileReader;
 import java.util.*;
 
+@SuppressWarnings({"rawtypes", "unchecked"})
 public class EpicEnsembleEvaluator implements IObjectEvaluator<ComponentInstance, Double> {
-    private final File ds;
+    private final List<Instances> split;
 
-    public EpicEnsembleEvaluator(File ds) {
-        this.ds = ds;
+    public EpicEnsembleEvaluator(ILabeledDataset dataset, Integer seed) throws SplitFailedException, InterruptedException {
+        // here it doesn't transform the dataset for every evaluation.
+        Instances instances = new WekaInstances(dataset).getInstances();
+        instances.setClassIndex(instances.numAttributes() - 1);
+        split = WekaUtil.getStratifiedSplit(instances, seed, .7);
     }
 
     @Override
     public Double evaluate(ComponentInstance ensemble) {
-        System.out.println("================================================");
-        try {
-            Instances dataset = new Instances(new FileReader(ds));
-            dataset.setClassIndex(dataset.numAttributes() - 1);
-            List<Instances> split = WekaUtil.getStratifiedSplit(dataset, new Random().nextLong(), .7);
+        List<IComponentInstance> nestedComponents = ensemble.getSatisfactionOfRequiredInterface("classifiers");
+        List<Classifier> classifiers = new ArrayList<>();
 
-            List<IComponentInstance> nestedComponents = ensemble.getSatisfactionOfRequiredInterface("classifiers");
-            System.out.println("Ensemble with " + nestedComponents.size() + " nested components: ");
-            nestedComponents.forEach(iComponentInstance -> System.out.println("\t" + iComponentInstance.getComponent().getName()));
-            List<Classifier> classifiers = new ArrayList<>();
-
-            for (IComponentInstance nestedComponent : nestedComponents) {
-                try {
-                    Classifier classifier = (Classifier) Class.forName(nestedComponent.getComponent().getName()).newInstance();
-                    classifier.buildClassifier(split.get(0));
-                    classifiers.add(classifier);
-                } catch (UnsupportedAttributeTypeException e) {
-                    System.out.println("Ignored component: " + nestedComponent.getComponent().getName() + " from ensemble.");
-                }
+        for (IComponentInstance nestedComponent : nestedComponents) {
+            try {
+                Classifier classifier = (Classifier) Class.forName(nestedComponent.getComponent().getName()).newInstance();
+                classifier.buildClassifier(split.get(0));
+                classifiers.add(classifier);
+            } catch (UnsupportedAttributeTypeException e) {
+                // do nothing. This is for skipping classifiers that don't work on the dataset.
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            int mistakes = 0;
-            // make ensemble prediction for every instance in instances
-            for (Instance instance : split.get(1)) {
-                Map<Double, Integer> classPredictions = new HashMap<>();
-                // predict with every nested component, reminder that if a nested component throws an exception, the whole ensemble is discarded (instead of that particular component)
-                for (Classifier classifier : classifiers) {
-                    Double prediction = classifier.classifyInstance(instance);
-                    if (classPredictions.containsKey(prediction)) {
-                        classPredictions.compute(prediction, (aDouble, integer) -> integer + 1);
-                    } else {
-                        classPredictions.put(prediction, 1);
-                    }
-                }
-
-                // find most frequent prediction and use it as the ensemble's prediction (hard voting)
-                Double ensemblePrediction = classPredictions.entrySet().iterator().next().getKey(); // first key
-                for (Map.Entry<Double, Integer> entry : classPredictions.entrySet()) {
-                    if (!entry.getKey().equals(ensemblePrediction))
-                        if (entry.getValue() > classPredictions.get(ensemblePrediction))
-                            ensemblePrediction = entry.getKey();
-                }
-
-                if (ensemblePrediction != instance.classValue()) {
-                    mistakes++;
-                }
-            }
-            Double score = mistakes * 100.0 / split.get(1).size();
-            System.out.println("Considered ensemble with score " + score);
-            return score;
-        } catch (Exception e) {
-            System.out.println("Ignored ensemble with message " + e.getMessage());
         }
-        return Double.MAX_VALUE; // disqualify ensemble if something weird happens...
+
+        EpicEnsemble epicEnsemble = null;
+        try {
+            epicEnsemble = new EpicEnsemble(classifiers, split.get(0));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        int mistakes = 0;
+        // make ensemble prediction for every instance in instances
+        for (Instance instance : split.get(1)) {
+            double ensemblePrediction = 0;
+            try {
+                ensemblePrediction = epicEnsemble.classifyInstance(instance);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (ensemblePrediction != instance.classValue()) {
+                mistakes++;
+            }
+        }
+        double score = mistakes * 100.0 / split.get(1).size();
+        for (int i = 0; i < epicEnsemble.getClassifiers().length; i++) {
+            System.out.println(epicEnsemble.getClassifiers()[i].getClass());
+        }
+        System.out.println("Considered one ensemble with score: " + score);
+        return score;
     }
 }
--- src/test/java/xyz/acevedosharp/EpicPerformanceMeasurer.java
+++ src/test/java/xyz/acevedosharp/EpicPerformanceMeasurer.java
@@ -1,13 +1,13 @@
 package xyz.acevedosharp;
 
 import ai.libs.hasco.core.events.HASCOSolutionEvent;
+import ai.libs.jaicore.components.api.IComponentInstance;
 import ai.libs.jaicore.components.model.ComponentInstance;
 import ai.libs.jaicore.components.model.RefinementConfiguredSoftwareConfigurationProblem;
 
 import java.io.*;
 import java.net.URISyntaxException;
 import java.sql.*;
-import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
@@ -21,7 +21,8 @@ import ai.libs.jaicore.ml.core.dataset.serialization.ArffDatasetAdapter;
 import ai.libs.jaicore.ml.core.evaluation.evaluator.SupervisedLearnerExecutor;
 import ai.libs.jaicore.ml.core.filter.SplitterUtil;
 import ai.libs.jaicore.ml.weka.classification.learner.IWekaClassifier;
-import ai.libs.mlplan.core.MLPlan;
+import ai.libs.jaicore.ml.weka.classification.learner.WekaClassifier;
+import ai.libs.jaicore.ml.weka.dataset.WekaInstances;
 import ai.libs.mlplan.weka.MLPlanWekaBuilder;
 
 import org.api4.java.ai.ml.classification.singlelabel.evaluation.ISingleLabelClassification;
@@ -38,61 +39,79 @@ import org.api4.java.algorithm.exceptions.AlgorithmExecutionCanceledException;
 import org.api4.java.algorithm.exceptions.AlgorithmTimeoutedException;
 import org.api4.java.common.attributedobjects.IObjectEvaluator;
 import org.junit.Test;
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+import weka.core.UnsupportedAttributeTypeException;
 
 import java.io.File;
 import java.util.stream.Collectors;
 
-@SuppressWarnings({"UnusedLabel", "unchecked", "ConstantConditions", "rawtypes", "ForLoopReplaceableByForEach"})
+@SuppressWarnings({"unchecked", "ConstantConditions", "rawtypes"})
 public class EpicPerformanceMeasurer {
 
     private static final List<String> DATASET_NAMES = Arrays.asList("datasets/iris.arff");
-    private static final Timeout TIMEOUT = new Timeout(1, TimeUnit.HOURS);
+    private static final Timeout TIMEOUT = new Timeout(240, TimeUnit.SECONDS);
+    private static final int REPETITIONS = 2;
 
     @Test
-    public void epicHascoRunner() throws IOException, URISyntaxException, SQLException, InterruptedException, AlgorithmTimeoutedException, AlgorithmExecutionCanceledException, AlgorithmException {
-        List<File> datasets = datasetsAsFiles();
+    public void epicHascoRunner() throws Exception {
+        List<ILabeledDataset<?>> datasets = loadLabeledDatasets();
+        for (int i = 0; i < DATASET_NAMES.size(); i++) {
+            for (int j = 1; j <= REPETITIONS; j++) {
+                // split dataset
+                List<ILabeledDataset<?>> split = SplitterUtil.getLabelStratifiedTrainTestSplit(datasets.get(i), j, .7);
 
-        for (int i = 0; i < datasets.size(); i++) {
-            for (int j = 1; j <= 5; j++) {
-                runHasco(datasets.get(i), i, j);
+                // run the algorithm with .7 from above
+                IWekaClassifier result = runHasco(split.get(0),i , j);
+
+                // evaluate its result
+                Double performance = measureAlgorithmPerformance(result, split.get(1));
+
+                // write result to database
+                writeToDatabase(new BenchmarkResult(
+                        DATASET_NAMES.get(i),
+                        Algos.HASCO,
+                        performance,
+                        j
+                ));
             }
         }
     }
 
     @Test
-    public void epicMLPlanRunner() throws SplitFailedException, URISyntaxException, DatasetDeserializationFailedException, InterruptedException, IOException {
-        List<ILabeledDataset<?>> datasets = labeledDatasets();
+    public void epicMLPlanRunner() throws SplitFailedException, InterruptedException, IOException, SQLException, LearnerExecutionFailedException, AlgorithmTimeoutedException, AlgorithmExecutionCanceledException, AlgorithmException {
+        List<ILabeledDataset<?>> datasets = loadLabeledDatasets();
+        for (int i = 0; i < DATASET_NAMES.size(); i++) {
+            for (int j = 1; j <= REPETITIONS; j++) {
+                // split dataset
+                List<ILabeledDataset<?>> split = SplitterUtil.getLabelStratifiedTrainTestSplit(datasets.get(i), j, .7);
 
-        for (int i = 0; i < datasets.size(); i++) {
-            for (int j = 1; j <= 5; j++) {
-                runMLPlan(datasets.get(i), i, j);
+                // run the algorithm with .7 from above
+                IWekaClassifier optimizedClassifier = runMLPlan(split.get(0));
+
+                // evaluate its result with .3
+                Double performance = measureAlgorithmPerformance(optimizedClassifier, split.get(1));
+
+                // SUGGESTION: store results for every classifier and compare that against the ensemble
+
+                // write result to database
+                writeToDatabase(new BenchmarkResult(
+                        DATASET_NAMES.get(i),
+                        Algos.MLPLAN,
+                        performance,
+                        j
+                ));
             }
         }
     }
 
-    @Test
-    public void testDb() throws SQLException {
-        writeToDatabase(new BenchmarkResult("monke", Algos.HASCO, 10.4, 1));
-    }
+    private IWekaClassifier runHasco(ILabeledDataset dataset, int i, int j) throws Exception {
+        System.out.println("Execution of HASCO #"+j+" on dataset: "+ DATASET_NAMES.get(i) + " began at "+ LocalDateTime.now()+".");
 
-    @SuppressWarnings("SqlResolve")
-    private void writeToDatabase(BenchmarkResult result) throws SQLException {
-        Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/epic", "root", "password"
-        );
-        Statement statement = connection.createStatement();
-
-        int res = statement.executeUpdate("insert into benchmarkresult (dataset, algo, error_rate, repetition, moment_saved) value ('" +
-                result.datasetSource + "', '" + result.algorithm.toString() + "', " + result.errorRate + ", " + result.repetition + ", " + System.currentTimeMillis() +
-                ");");
-        System.out.println(res);
-    }
-
-    private void runHasco(File dsFile, int i, int j) throws URISyntaxException, IOException, SQLException, InterruptedException, AlgorithmTimeoutedException, AlgorithmExecutionCanceledException, AlgorithmException {
         String reqInterface = "EpicEnsemble";
         File componentFile = new File(this.getClass().getClassLoader().getResource("search-space/ensemble-configuration.json").toURI());
 
-        IObjectEvaluator<ComponentInstance, Double> evaluator = new EpicEnsembleEvaluator(dsFile);
+        IObjectEvaluator<ComponentInstance, Double> evaluator = new EpicEnsembleEvaluator(dataset, j);
 
         RefinementConfiguredSoftwareConfigurationProblem<Double> problem =
                 new RefinementConfiguredSoftwareConfigurationProblem(componentFile, reqInterface, evaluator);
@@ -100,12 +119,11 @@ public class EpicPerformanceMeasurer {
         HASCOViaFD<Double> hasco = HASCOBuilder.get()
                 .withProblem(problem)
                 .withBestFirst()
+                .withCPUs(2)
+                .withTimeout(TIMEOUT)
                 .withRandomCompletions()
                 .withDefaultParametrizationsFirst()
                 .getAlgorithm();
-
-        hasco.setTimeout(TIMEOUT);
-
 
         ArrayList<HASCOSolutionCandidate<Double>> solutions = new ArrayList<>();
 
@@ -126,68 +144,40 @@ public class EpicPerformanceMeasurer {
 
         HASCOSolutionCandidate<Double> solution = solutions.get(0);
         for (HASCOSolutionCandidate<Double> s : solutions) {
-            if (s.getScore() > solution.getScore())
+            if (s.getScore() < solution.getScore())
                 solution = s;
         }
 
-        writeToDatabase(new BenchmarkResult(
-                DATASET_NAMES.get(i),
-                Algos.HASCO,
-                solution.getScore(), // equal to error rate here
-                j
-        ));
-    }
+        // transform solution into IWekaClassifier
+        // very wasteful and not DRY, but it's okay since this happens outside of HASCO's runtime and doesn't affect the timeout.
+        Instances instances = new WekaInstances(dataset).getInstances();
+        List<IComponentInstance> nestedComponents = solution.getComponentInstance().getSatisfactionOfRequiredInterface("classifiers");
+        List<Classifier> classifiers = new ArrayList<>();
 
-    private void runMLPlan(ILabeledDataset ds, int i, int j) throws IOException, SplitFailedException, InterruptedException, DatasetDeserializationFailedException, URISyntaxException {
-
-        List<ILabeledDataset<?>> split = SplitterUtil.getLabelStratifiedTrainTestSplit(ds, new Random(42), .7);  //TODO: parametrize seed
-        MLPlan<IWekaClassifier> mlplan = new MLPlanWekaBuilder().withNumCpus(2).withTimeOut(TIMEOUT).withDataset(split.get(0)).build();
-
-        long start;
-        IWekaClassifier optimizedClassifier;
-        try {
-            start = System.currentTimeMillis();
-            optimizedClassifier = mlplan.call();
-            long trainTime = System.currentTimeMillis() - start;
-            System.out.println("Finished build of the classifier. Training time was " + trainTime + "s");
-            System.out.println("Chosen model is: " + mlplan.getSelectedClassifier());
-
-            /* evaluate solution produced by mlplan */
-            SupervisedLearnerExecutor executor = new SupervisedLearnerExecutor();
-            ILearnerRunReport report = executor.execute(optimizedClassifier, split.get(1));
-            double errorRate = EClassificationPerformanceMeasure.ERRORRATE.loss(report.getPredictionDiffList().getCastedView(Integer.class, ISingleLabelClassification.class));
-            System.out.println("Error Rate of the solution produced by ML-Plan: " +
-                    errorRate +
-                    ". Internally believed error was " +
-                    mlplan.getInternalValidationErrorOfSelectedClassifier()
-            );
-
-            writeToDatabase(new BenchmarkResult(
-                    DATASET_NAMES.get(i),
-                    Algos.MLPLAN,
-                    errorRate,
-                    j
-            ));
-        } catch (NoSuchElementException | AlgorithmException | InterruptedException | AlgorithmExecutionCanceledException | AlgorithmTimeoutedException | LearnerExecutionFailedException | SQLException e) {
-            System.out.println("Building the classifier failed: " + e.getMessage());
+        for (IComponentInstance nestedComponent : nestedComponents) {
+            try {
+                Classifier classifier = (Classifier) Class.forName(nestedComponent.getComponent().getName()).newInstance();
+                classifier.buildClassifier(instances);
+                classifiers.add(classifier);
+            } catch (UnsupportedAttributeTypeException e) {
+                System.out.println("Ignored component: " + nestedComponent.getComponent().getName() + " from ensemble.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
+        EpicEnsemble epicEnsemble = new EpicEnsemble(classifiers, instances);
+
+        System.out.println("Execution of HASCO ended at "+ LocalDateTime.now()+".");
+        return new WekaClassifier(epicEnsemble);
     }
 
-    private List<File> datasetsAsFiles() {
-        return DATASET_NAMES.stream().map(
-                s -> {
-                    try {
-                        return new File(this.getClass().getClassLoader().getResource(s).toURI());
-                    } catch (URISyntaxException e) {
-                        e.printStackTrace();
-                    }
-                    return null;  // shouldn't happen
-                }
-        ).collect(Collectors.toList());
+    private IWekaClassifier runMLPlan(ILabeledDataset dataset) throws IOException, InterruptedException, AlgorithmExecutionCanceledException, AlgorithmTimeoutedException, AlgorithmException {
+        // train with the entire dataset (0.7 from runner), the split and benchmarking is handled in the runner.
+        return new MLPlanWekaBuilder().withNumCpus(2).withTimeOut(TIMEOUT).withDataset(dataset).build().call();
     }
 
-    private List<ILabeledDataset<?>> labeledDatasets() {
+    private List<ILabeledDataset<?>> loadLabeledDatasets() {
         return DATASET_NAMES.stream().map(
                 s -> {
                     try {
@@ -199,5 +189,23 @@ public class EpicPerformanceMeasurer {
                     return null; // shouldn't happen
                 }
         ).collect(Collectors.toList());
+    }
+
+    private Double measureAlgorithmPerformance(IWekaClassifier optimizedClassifier, ILabeledDataset dataset) throws LearnerExecutionFailedException {
+        SupervisedLearnerExecutor executor = new SupervisedLearnerExecutor();
+        ILearnerRunReport report = executor.execute(optimizedClassifier, dataset); // 0.3 from runner
+        return EClassificationPerformanceMeasure.ERRORRATE.loss(report.getPredictionDiffList().getCastedView(Integer.class, ISingleLabelClassification.class));
+    }
+
+    @SuppressWarnings("SqlResolve")
+    private void writeToDatabase(BenchmarkResult result) throws SQLException {
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/epic", "root", "password"
+        );
+        Statement statement = connection.createStatement();
+
+        statement.executeUpdate("insert into benchmarkresult (dataset, algo, error_rate, repetition, moment_saved, date_string) value ('" +
+                result.datasetSource + "', '" + result.algorithm.toString() + "', " + result.errorRate + ", " + result.repetition + ", " + System.currentTimeMillis() + "," + LocalDateTime.now().toString() +
+                ");");
     }
 }
